1faa7c8da976a92600561754f6c6d13997f3888f
Filipe <falcoefilipe@hotmail.com>
2020-05-24 14:49:39 -0400

    inserido linkagem nos cards

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,27 +1,33 @@
 @extends('site.master.layout')
 
 @section('content')
-    <div class="row">
-        <div class="col s12 l8 offset-l3">
+<div class="row">
+    <div class="col s12 l8 offset-l3">
+        <a href="">
             <div class="card cards-correction">
                 <img class="image-corrector" src="{{ asset('images/target.png') }}">
                 <h5>Recursos Humanos</h5>
                 <p class="card-text-format">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, facere!</p>
             </div>
-        </div>
-        <div class="col s12 l8 offset-l3">
+        </a>
+    </div>
+    <div class="col s12 l8 offset-l3">
+        <a href="">
             <div class="card cards-correction">
                 <img class="image-corrector" src="{{ asset('images/technical.png') }}">
                 <h5>Departamento de informatica</h5>
                 <p class="card-text-format">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, atque!</p>
             </div>
-        </div>
-        <div class="col s12 l8 offset-l3">
+        </a>
+    </div>
+    <div class="col s12 l8 offset-l3">
+        <a href="">
             <div class="card cards-correction">
                 <img class="image-corrector" src="{{ asset('images/payment-terminal.png') }}">
                 <h5>Requisições de mercado</h5>
                 <p class="card-text-format">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, commodi.</p>
             </div>
-        </div>
     </div>
+</div>
+</div>
 @endsection
