0619ba65b9c640c452ed68bfb19aa444bfb7dcd8
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2020-06-28 18:38:02 -0400

    correção do menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,25 +1,35 @@
-@extends('layout.blade.php')
+@extends('master.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="row">
+    <div class="col s12 l10 offset-l2">
+        <a href="">
+            <div class="card responsive-img center-align">
+                <img class="image-corrector" src="{{ asset('images/target.png') }}">
+                <h5>Recursos Humanos</h5>
+                <p class="card-text-format">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, facere!</p>
+            </div>
+        </a>
+    </div>
+    <div class="col s12 l10 offset-l2">
+        <a href="">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="responsive-img center-align">
+                    <img class="image-corrector" src="{{ asset('images/technical.png') }}">
+                    <h5>Departamento de informatica</h5>
+                    <p class="card-text-format">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, atque!</p>
                 </div>
             </div>
-        </div>
+        </a>
+    </div>
+    <div class="col s12 l10 offset-l2">
+        <a href="">
+            <div class="card responsive-img center-align">
+                <img class="image-corrector" src="{{ asset('images/payment-terminal.png') }}">
+                <h5>Requisições de mercado</h5>
+                <p class="card-text-format">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, commodi.</p>
+            </div>
     </div>
 </div>
+</div>
 @endsection
-
-
